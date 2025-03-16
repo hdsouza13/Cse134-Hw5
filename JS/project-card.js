ab99553120224372b5d78385c4d45b80d1705f14
hdsouza13 <hdsouza@ucsd.edu>
2025-03-15 23:56:36 -0700

    Up

--- JS/project-card.js
+++ JS/project-card.js
@@ -94,49 +94,49 @@ class ProjectCard extends HTMLElement {
 
 customElements.define('project-card', ProjectCard);
 
-const localData = [
+const projects = [
     {
-        project: "Regency Young Scholar Science Program",
-        imgSrc: "../IMG_0021.jpg",
-        imgAlt: "Regency group image",
-        description: "The Regency Young Scholar Science Program aims to bridge the gap in science education for children who may not have access to hands-on learning experiences. Through engaging experiments and activities, the program introduces fundamental scientific principles in an accessible and fun way. By fostering curiosity and exploration, it not only enhances scientific knowledge but also nurtures critical thinking and problem-solving skills from an early age. The program strives to inspire a lifelong passion for science, encouraging children to see the world through an analytical lens and empowering them to become future innovators and leaders in their communities. I was one of the individuals responsible for teaching the kids, guiding them through experiments and activities to help them develop a deeper understanding of scientific concepts.",
-        link: "https://childcarecenter.us/provider_detail/young_scholars_academy_draper_ut"
-    },
-    {
-        project: "Website Project",
-        imgSrc: "../weblogo.png",
-        imgAlt: "Web page logo",
-        description: "This video showcases a preliminary design of the website project that I participated in. My group and I created a DevJournal website based on the design we developed. The website was built to provide a structured and engaging platform for developers to document their projects, share insights, and collaborate. From conceptualizing the user interface in Figma to implementing the final web application, our team worked together to bring the design to life, ensuring both functionality and an intuitive user experience.",
-        link: "webvideo.html"
-    },
-    {
-        project: "Nasa California Space Grant Consortium",
-        imgSrc: "../nasalogo.png",
-        imgAlt: "Nasa logo",
-        description: "The Lunar Lava Tube Exploration challenge tasks teams with exploring a lunar lava tube, assessing its potential for human habitation. The challenge involves building a rover and developing programs that enable the rover to navigate a model lunar lava tube. I was responsible for the coding aspect of this project, implementing the navigation programs using Arduino to control the rover's movement and decision-making in the simulated environment.",
-        link: "https://casgc.ucsd.edu/"
-    },
-    {
-        project: "OCC Caculus Tutor",
-        imgSrc: "../occlogo.png",
-        imgAlt: "OCC logo",
-        description: "Tutored students in Calculus 1, 2, and 3 at Orange Coast College, supporting academic success through personalized instruction and targeted problem-solving strategies.",
-        link: "https://orangecoastcollege.edu/services-support/tutoring-services/"
-    },
-    {
-        project: "Tennis Assistant",
-        imgSrc: "../tennislogo.png",
-        imgAlt: "Tennis logo",
-        description: "Assisted my coach in teaching a group of 10 children from a community to play tennis. Enhanced player performance by providing individualized coaching and targeted training drills.",
-        link: "https://www.clubepitangueiras.com.br/tenis.html"
-    }
-];
+        "project": "Regency Young Scholar Science Program",
+        "imgSrc": "../IMG_0021.jpg",
+        "imgAlt": "Regency group image",
+        "description": "The Regency Young Scholar Science Program aims to bridge the gap in science education for children who may not have access to hands-on learning experiences...",
+        "link": "https://childcarecenter.us/provider_detail/young_scholars_academy_draper_ut"
+      },
+      {
+        "project": "Website Project",
+        "imgSrc": "../weblogo.png",
+        "imgAlt": "Web page logo",
+        "description": "This video showcases a preliminary design of the website project that I participated in...",
+        "link": "webvideo.html"
+      },
+      {
+        "project": "Nasa California Space Grant Consortium",
+        "imgSrc": "../nasalogo.png",
+        "imgAlt": "Nasa logo",
+        "description": "The Lunar Lava Tube Exploration challenge tasks teams with exploring a lunar lava tube...",
+        "link": "https://casgc.ucsd.edu/"
+      },
+      {
+        "project": "OCC Caculus Tutor",
+        "imgSrc": "../occlogo.png",
+        "imgAlt": "OCC logo",
+        "description": "Tutored students in Calculus 1, 2, and 3 at Orange Coast College...",
+        "link": "https://orangecoastcollege.edu/services-support/tutoring-services/"
+      },
+      {
+        "project": "Tennis Assistant",
+        "imgSrc": "../tennislogo.png",
+        "imgAlt": "Tennis logo",
+        "description": "Assisted my coach in teaching a group of 10 children from a community to play tennis...",
+        "link": "https://www.clubepitangueiras.com.br/tenis.html"
+      }
+    ];
 
-localStorage.setItem('projects', JSON.stringify(localData));
+localStorage.setItem('projects', JSON.stringify(projects));
 
 function loadLocal() {
     const projectsContainer = document.getElementById('projects-container');
-    const projects = JSON.parse(localStorage.getItem('project'));
+    const projects = JSON.parse(localStorage.getItem('projects'));
 
     if (projects && projects.length > 0) {
         projectsContainer.innerHTML = '';
@@ -153,14 +153,12 @@ function loadLocal() {
         });
     }
 }
+
 function loadRemote() {
     const projectsContainer = document.getElementById('projects-container');
-    fetch('https://my-json-server.typicode.com/hdsouza13/Cse134-hw5db/projects')
+    fetch('https://my-json-server.typicode.com/hdsouza13/Cse134-hw5db/db')
         .then(response => response.json())
-        .then(data => {
-            console.log(data); // Debugging: log the entire response
-            const projects = data.projects; // Access the 'projects' key
-            console.log(projects); // Debugging: log the projects array
+        .then(projects => {
             if (projects && projects.length > 0) {
                 projectsContainer.innerHTML = '';
                 projects.forEach(project => {
